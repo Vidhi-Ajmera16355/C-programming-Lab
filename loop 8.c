84243dc4f60b52c0cf3cd1d03203ae0828a14619
Vidhi Ajmera <113877644+Vidhi-Ajmera16355@users.noreply.github.com>
2023-03-17 08:38:32 +0530

    Create loop 8.c

---
+++ loop 8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,sum=0;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    for (int i=1;i<=n;i++){
+        if (i%2!=0){
+         sum=sum+i;   
+        }
+    }
+    printf("Sum of all odd natural number from 1 to n:%d\n",sum);
+    return 0;
+}
