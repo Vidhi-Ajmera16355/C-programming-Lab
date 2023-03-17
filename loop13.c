7c594a9857c1f1a089523aa2ab19600d34cd376d
Vidhi Ajmera <113877644+Vidhi-Ajmera16355@users.noreply.github.com>
2023-03-17 09:19:06 +0530

    Create loop13.c

---
+++ loop13.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int first,second,temp;
+    printf("Enter first number:\n");
+    scanf("%d",&first);
+    printf("Enter second number:\n");
+    scanf("%d",&second);
+   temp=first;
+   first=second;
+   second=temp;
+   printf("After swapping first number %d\n",first);
+   printf("After swapping second number %d",second);
+    return 0;
+}
