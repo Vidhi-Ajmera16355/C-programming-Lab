9f288c320e384ecd6d70e57cd5ab2d6df76129c1
Vidhi Ajmera <113877644+Vidhi-Ajmera16355@users.noreply.github.com>
2023-03-21 13:40:20 +0530

    Create pattern7.c
    10001
    01010
    00100
    01010
    10001

---
+++ pattern7.c
@@ -0,0 +1,19 @@
+/**
+ * C program to print box number pattern with plus in center
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int i,j,n;
+    scanf("%d",&n);
+    for (i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            if ((i==j)||(i+j==n+1)) printf("1");
+            else printf("0");
+        }
+        printf("\n");
+    }
+    return 0;
+}
