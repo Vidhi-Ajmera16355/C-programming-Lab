0af3beb810c9a4aee96982ef1008fd4ba94fdca4
Vidhi Ajmera <113877644+Vidhi-Ajmera16355@users.noreply.github.com>
2023-03-21 15:32:56 +0530

    Create pattern20.c
    55555
    4444
    333
    22
    1

---
+++ pattern20.c
@@ -0,0 +1,24 @@
+/**
+ * C program to print number pattern
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int N, i, j;
+
+    printf("Enter N: ");
+    scanf("%d", &N);
+
+    for(i=N; i>=1; i--)
+    {
+       for (j=1;j<=i;j++){
+           printf("%d",i);
+       }
+
+        printf("\n");
+    }
+
+    return 0;
+}
