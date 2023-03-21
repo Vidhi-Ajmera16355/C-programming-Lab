8d111384950ac681268bac7cafb89366e661ba8c
Vidhi Ajmera <113877644+Vidhi-Ajmera16355@users.noreply.github.com>
2023-03-21 14:29:01 +0530

    Create pattern14.c
    12345
    23455
    34555
    45555
    55555

---
+++ pattern14.c
@@ -0,0 +1,20 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int i,j,n;
+    scanf("%d",&n);
+    for (i=1;i<=n;i++){
+        for(j=i; j<=n; j++)
+        {
+            printf("%d", j);
+        }
+        for (j=i;j>1;j--){
+            printf("%d",n);
+        }
+        printf("\n");
+    }
+    return 0;
+}
